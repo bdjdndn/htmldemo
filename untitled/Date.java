65e53938bfcd855598da9b2426a79346672f29c5
useryang <909439250@qq.com>
2022-05-11 09:05:14 +0800

    第一次提交 修改导航栏为挂件
    Signed-off-by: useryang <909439250@qq.com>

---
+++ untitled/Date.java
@@ -0,0 +1,8 @@
+public class Date {
+    public static void main(String[] args) {
+        int i =0
+                for(i<=100, i++){
+                    Systeam.out.println(i)
+                }
+    }
+}
